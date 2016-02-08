53c2148aa63f25a493a060312fe614039f15c6ff
Jakob Sunde <jsunde@uw.edu>
2016-02-08 00:19:35 -0800

    Switched from Jasmine to QUnit for our testing framework

--- RoomEasePhoneGapProject/www/tests/indexSpec.js
+++
@@ -1,8 +0,0 @@
-var $ = require("../js/lib/jquery-2.1.3.min.js");
-//var re = require("../js/re.js");
-
-describe("Example suite", function() {
-    it("has successfully loaded re.js", function() {
-        expect($).toBeTruthy();
-    });
-});
