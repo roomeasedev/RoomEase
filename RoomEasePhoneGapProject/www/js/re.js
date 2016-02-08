62a6e04175f1631b8ea9ca8bcb5a82749b0cb080
Alexander Vrhel <avrhel@cs.washington.edu>
2016-02-07 16:41:18 -0800

    Added comments to re.js and template-loader.js

--- RoomEasePhoneGapProject/www/js/re.js
+++ RoomEasePhoneGapProject/www/js/re.js
@@ -1,26 +1,47 @@
-// Define a global object, re, on which other namespaces
-// will be created.
+"use strict";
+/**
+* This file is the base javascript file which initializes
+* the other javascript modules. The application uses a single-page
+* architecture, so this file also contains the logic to render the
+* correct view to the user based on the current routing of the page
+* (the hash of the URL).
+*/
+
+// Define a global object, re, to use as a namespace. re is the namespace
+// on which other namespaces will be created.
 var re = {};
 
+/**
+* Function to be called when the page has loaded.  Defines the various
+* rendering functions, implements the logic to listen for changes to the
+* URL hash and render the correct view accordingly. Loads (compiles) all
+* of the templates for the various views so they are ready to be displayed.
+*/
 $(document).ready(function () {
-    
-    "use strict";
-    console.log("test");
+    // Define various templates, which hold the compiled templates for each of the views.
     var feedTemplate, listTemplate;
     
+    /**
+    * Sets the HTML value of the injectable page area to the rendered list view.
+    */
     function renderListView() {
         $('.page').html(listTemplate());
     }
     
+    /**
+    * Sets the HTML value of the injectable page area to the rendered feed view.
+    */
     function renderFeedView() {
         $('.page').html(feedTemplate());
     }
     
-    // Renders the correct view by the value of the
-    // hash of the URL.
+    /**
+    * Renders the correct view for the injectable area of the viewport.
+    * Uses the current hash of the URL to determine which view should be
+    * rendered, then calls the appropriate rendering function.
+    */
     function route() {
         var hash = window.location.hash;
-        var match;
         console.log(hash);
         if (!hash || hash == "#feed") {
             renderFeedView();
@@ -33,11 +54,10 @@ $(document).ready(function () {
     // when the hash of the URL is changed.
     $(window).on('hashchange', route);
     
-    $('button').on('click', function(e) {
-        //console.log(window.location.hash);
-
-    });
-    
+    // Call the load function of re.templates, compiling the HTML templates
+    // for all existing views.  When load finishes, set the values of the
+    // template variables to store the appropriate compiled templates. Finally,
+    // route the viewport to the correct view based on the current hash.
     re.templates.load(["Feed", "List"]).done(function () {
         feedTemplate = re.templates.get("Feed");
         listTemplate = re.templates.get("List");
--- RoomEasePhoneGapProject/www/js/template-loader.js
+++ RoomEasePhoneGapProject/www/js/template-loader.js
@@ -1,18 +1,28 @@
-// Defines a template loader which can load (compile) all templates for each
-// of the main views.  re.templates is defined as an object with 2 properties,
-
-//
+"use strict";
+/**
+* re.templates is a module which compiles the HTML templates for each of the views
+* of the application.  It stores the compiled templates and allows the main page to
+* get the compiled templates from re.template's storage.
+* @return {Object} An object representing re.templates, which has a "load" and "get"
+*     function. Load allows a client to compile a template of the given name(s), while
+*     get allows a client to retrieve the compiled template they specify, provided it
+*     was already loaded by calling the load function.
+*/
 re.templates = (function () {
-    
-    "use strict";
-    
-    var templates = {};  // Object to hold the compiled templates for each view
+    /**
+    * Representation invariant: templates is defined and != null
+    *   and for each <name> passed as an argument to load for which
+    *   there is a valid <name>.html file, templates[<name>] = the
+    *   HTML template compiled from <name>.html
+    */
+    var templates = {}; 
     
     /**
     * Loads the templates with the given filenames.  Compiles them using
     * HandlebarsJS so they are ready to be displayed when the client calls
     * the get function on a particular template.
-    * @param arguments (JS function has an inherent arguments)
+    * @param {Array} templateNames an array of names, where each name is a string for
+    *     which there is a valid <name>.html file in the templates directory
     */
     var load = function (templateNames) {
         var deferred = [];
@@ -26,7 +36,13 @@ re.templates = (function () {
         return $.when.apply(null, deferred);
     };
     
-    // Gets the compiled template for the given name. 
+    /**
+    * Get returns the compiled HTML template that was loaded from the specified
+    * file name (templates/<name>.html).
+    * @param {String} name the name of the compiled template the client is requesting.
+    * @return {String} the compiled template for the given template name, or undefined
+    *     if the client has not yet called load with the given name as an argument
+    */
     var get = function(name) {
         return templates[name];
     };
