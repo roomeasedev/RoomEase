1ac3a348508c55e4e10ee5a0f6741468cd7e5d15
cher-wang <cwang7@uw.edu>
2016-02-16 14:48:55 -0800

    refactored add list logic into controller

--- RoomEasePhoneGapProject/www/js/controller.js
+++ RoomEasePhoneGapProject/www/js/controller.js
@@ -16,29 +16,47 @@ re.controller = (function() {
 		console.log("re.controller init finished!");
 	}
     
+    /* Onclick function for new list button
+     *
+     */
+    function makeNewList() {
+        $('#new-list-btn').css('display', 'none');
+        $('.new-list-popup').css('display', 'block');
+                
+        // stores the new list in the database on "Save" button click
+        $('#store').click(function() {
+            // need to pass in name-of-list, text, items, dummy varibles for visible/modifiable users for now
+            $('#new-list-btn').css('display', 'block');
+            $('.new-list-popup').css('display', 'none');
+            var listName = $('#name').val();
+            var items = $('#items').val();
+            var text = $('#descrip-text').val();
+            re.controller.addListToDatabase(listName, items, text);
+            // TODO: put in some form of reloading
+            //       location.reload() doesn't work; lists won't ever be displayed even if in database
+        });
+
+        // clears the fields in popup & closes it
+        $('#cancel').click(function() {
+            $('#new-list-btn').css('display', 'block');
+            $('.new-list-popup').css('display', 'none');
+            $('#name').val('');
+            $('#items').val('');
+            $('#descrip-text').val('');
+        });
+    }
+    
     /* creates a JSON list object to add to database & handles any resulting errors
      * name: the name of the list
      * items: string of items to put into list
      */
-    function addList(listName, items, text) {
-        var newlist = {
-            "type": "list",
-            "name_of_list": listName,
-            "text": text,
-            // TODO: not sure what to split on -- all whitespace may be incorrect
-            "items": items.split(" "),
-            "visible_users":
-                ["12345567878", //Hardcoding in IDs for now
-                    "124444433333"], 
-            "modifiable_users":
-                ["12344444", //Hardcoded
-                "1124444444"]
-        }
-        
+    function addListToDatabase(listName, items, text) {
+        var newlist = re.controller.createList(listName, items, text);        
         re.requestHandler.addItem(newlist, function(is_success, revised_item, error) { 
             if (is_success) {
                 console.log("successfully added list");
-                // TODO: reload the page
+                // TODO: reload
+                re.renderListView();
             } else {
                 console.log(error);
                 // let user know an error occurred and prompt them to try again
@@ -51,22 +69,37 @@ re.controller = (function() {
         });
     }
     
-    /* the onclick function called by a list item
+    /* Creates a JSON list object with listName, items, & text
+     *
+     */
+    function createList(listName, items, text) {
+        return list = {
+            "type": "list",
+            "name_of_list": listName,
+            "text": text,
+            // TODO: not sure what to split on -- all whitespace may be incorrect
+            "items": items.split(" "),
+            "visible_users":
+                ["12345567878", //Hardcoding in IDs for now
+                    "124444433333"], 
+            "modifiable_users":
+                ["12344444", //Hardcoded
+                "1124444444"]
+        }
+    }
+    
+    /* the longpress function
      *
      */
     function editList(listID) {
         window.alert("about to edit");
     }
     
-    /* registers a Handlebars helper 
-     *
-     */
-    Handlebars.registerHelper('json', function(context) {
-        return JSON.stringify(context);
-    });
-
 	return {
 		'init': init,
-        'addList': addList
+        'makeNewList': makeNewList,
+        'addListToDatabase': addListToDatabase,
+        'createList': createList,
+        'editList': editList
 	}
 })();
