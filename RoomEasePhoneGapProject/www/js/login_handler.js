5683268999ec55859dffcba6f593688bce5acb51
OmarAlSughayer <oasugher@gmail.com>
2016-02-16 18:00:55 -0800

    Generalized the parameters of login(callback)
    callback now has parameters ({user_name, user_id}, errorHandler) instead
    of (user_name, errorHandler}.

--- RoomEasePhoneGapProject/www/js/login_handler.js
+++ RoomEasePhoneGapProject/www/js/login_handler.js
@@ -5,8 +5,8 @@ re.loginHandler = (function() {
 
 	/**
     * attempts to log the user into their Facebook account
-    * @param {function(String, function)} the callback function
-    *     with parameters (user_id, errorHandler)
+    * @param {function({String, String}, function)} the callback function
+    *     with parameters ({user_name, user_id}, errorHandler)
     * @postcondition:  user have been logged into their facebook
     *     account and callbacl function have been invoked with 
     *     the right parameters
@@ -37,28 +37,34 @@ re.loginHandler = (function() {
 
     /**
     * gets the user's Facebook ID and invokes the callback appropriately
-    * @param {function(String, function)} the callback function
-    *     with parameters (user_id, errorHandler)
+    * @param {function({String, String}, function)} the callback function
+    *     with parameters ({user_name, user_id}, errorHandler)
     * @precondition:   user have been logged into their Facebook
     *     account successfully
     * @postcondition:  callback function have been invoked with 
     *     the right parameters
     */
     function getInfo(callback) {
-        //send a GET request to Facebook to retreave the user's ID
+        // send a GET request to Facebook to retreave the user's ID
         openFB.api({
             // HTTP method defaulted to GET
             path: '/me',
             success: function(data) {
+                      /** data is a JSON object on the form
+                      *	  {
+					  *		name: "John Wick"
+					  *		id:   "123456789"
+                      *   }
+                      */
                       console.log(JSON.stringify(data));
-                      var fbid = data.id;
-                      // alert user's id for testing purposes
-                      //alert(fbid);
-                      // if the call was successful call back with the id and no error
-                      callback(fbid, null);
+
+                      // alert user's data for testing purposes
+                      //alert(JSON.stringify(data));
+                      // if the call was successful call back with data and no error
+                      callback(data, null);
                     },
             error: function errorHandler(error) {
-                      // if the call was not successful call back with no id and 
+                      // if the call was not successful call back with no data and 
                       //     pass the error to callback function
                       callback(null, error);
                     }
