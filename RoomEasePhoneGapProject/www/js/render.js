e3182f901502df17b375521d7a9a742eb8fd8471
Alexander Vrhel <avrhel@cs.washington.edu>
2016-02-18 18:12:01 -0800

    Removed "let" keywords which were breaking iPhones
    Turns out that invalid javascript syntax will actually cause the entire
    file to not be loaded on some plattforms, so render.js was not being
    loaded.

--- RoomEasePhoneGapProject/www/js/render.js
+++ RoomEasePhoneGapProject/www/js/render.js
@@ -1,31 +1,32 @@
 "use strict";
-
 re.render = (function() {
     // Define various templates, which hold the compiled templates for each of the views.
-    var feedTemplate, listTemplate, fridgeTemplate,scheduleTemplate,
-        facebookLoginTemplate, groupLoginTemplate, choreTemplate;
+    var feedTemplate;
+    var listTemplate; 
+    var fridgeTemplate;
+    var scheduleTemplate;
+    var facebookLoginTemplate;
+    var groupLoginTemplate;
+    var choreTemplate;
     
     /**
     * Sets the HTML value of the injectable page area to the rendered list view.
     */
     function renderListView() {
         /* Gets all lists from database and renders the list view with these
-        *  lists embedded. 
+        *  lists embedded.
         */
         re.requestHandler.getAllItemsOfType('list', function(allLists, error) {
             if(allLists == null) {
                 console.log(error);
             } else {
-                for (let list of allLists) {
-                    re.controller.list_items[list._id] = list;
-                }
-//                re.controller.list_items = allLists;
+                re.controller.list_items = allLists;
             }
             
             $('.page-title').html('List');
             $('.page').html(listTemplate(re.controller.list_items));
             
-            for (let list of re.controller.list_items) {
+            for (list in re.controller.list_items) {
                 $('#' + list._id).longpress(function() {
                     re.controller.editList(list._id);
                 });
@@ -33,7 +34,7 @@ re.render = (function() {
         });
     }
 
-    function renderSchedulerView(){
+    function renderSchedulerView() {
         
         //TODO: Factor this out
         (function() {
@@ -51,14 +52,14 @@ re.render = (function() {
         
         var reservations;
         console.log("Rendering schedule view");
-         re.requestHandler.getAllItemsOfType('reservation', function(allReservations, error) {
+        re.requestHandler.getAllItemsOfType('reservation', function(allReservations, error) {
             if(allReservations == null) {
                 console.log(error);
             } else {
                 reservations = allReservations;
             }
             
-             
+        
             //Convert the date-time reservations int0 a more readable format
             var date_time_reservations = [];
             for(var i = 0; i < reservations.length; i++){
@@ -133,7 +134,7 @@ re.render = (function() {
              //Reservations based off of what they are
             $('.page').html(scheduleTemplate(date_time_reservations));
             
-            for (let reservation of reservations) {
+            for (reservation in reservations) {
                 $('#' + reservation._id).longpress(function() {
                     re.controller.editReservationItem(reservation._id);
                     console.log("Long press on reservation!");
@@ -183,9 +184,9 @@ re.render = (function() {
     function route() {
         var hash = window.location.hash;
         console.log(hash);
-        if (hash == "#fb") {
+        if (!hash || hash == "#fb") {
             renderFacebookLoginView();
-        } else if (!hash || hash == "#list") {      
+        } else if (hash == "#list") {      
             renderListView();
         } else if (hash == "#fridge") {
             renderFridgeView();
@@ -205,6 +206,8 @@ re.render = (function() {
     // template variables to store the appropriate compiled templates. Finally,
     // route the viewport to the correct view based on the current hash.
     function init() {
+        alert("called init");
+        console.log("init");
         re.templates.load(["Feed", "List", "Fridge", "Reservations", "Chores",
                            "FacebookLogin", "GroupLogin"]).done(function () {
             feedTemplate = re.templates.get("Feed");
