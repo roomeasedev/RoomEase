7035a965ebb71cb57861d631a5b587c58f937d8c
Matthew Mans <mans1626@uw.edu>
2016-02-25 01:45:16 -0800

    Forgot to remove git revision conflict.

--- RoomEasePhoneGapProject/www/js/controller.js
+++ RoomEasePhoneGapProject/www/js/controller.js
@@ -281,51 +281,6 @@ re.controller = (function() {
     *Function called make all of the resources visible to add a new fridge item in the Fridge tremplate
     **/
     function makeNewFridgeItem() {
-<<<<<<< HEAD
-//        // TODO: implement this method, which will bring up the popup to add an item,
-//        // call createNewFridgeItem to create the JSON, and then make the necessary requesthandler call
-//        
-//        $('#new-fridge-item-btn').css('display', 'none');
-//        $('.popupBackground').css('display', 'block');
-//        
-//        // TODO: Clear old info from popup
-//        
-//        // Adds the fridge item to the database when the next item button is pressed
-//        $('#next-item').click() {
-//            var itemName = $('#name').val();
-//            var expiration = $('expiration').val();
-//            var shared;
-//            if($('#yes_button').is(':checked')) {
-//                shared = "yes";
-//            } else if($('#no_button').is(':checked')) {
-//                shared = "no";
-//            } else {
-//                shared = "ask";
-//            }
-//            
-//            var newItem = createFridgeItem(itemName, expiration,);
-//            re.requestHandler.addItem(newItem, rhAddCallback);
-//        }
-//        
-//        // Adds the fridge item to the database when the done button is pressed and hides the popup
-//        $('#done').click(function() {
-//            // need to pass in name-of-list, text, items, dummy varibles for visible/modifiable users for now
-//            hidePopup();
-//            var itemName = $('#name').val();
-//            var expiration = $('expiration').val();
-//            var shared;
-//            if($('#yes_button').is(':checked')) {
-//                shared = "yes";
-//            } else if($('#no_button').is(':checked')) {
-//                shared = "no";
-//            } else {
-//                shared = "ask";
-//            }
-//            
-//            var newItem = createFridgeItem(itemName, expiration,);
-//            re.requestHandler.addItem(newItem, rhAddCallback);
-//        });
-=======
         // TODO: implement this method, which will bring up the popup to add an item,
         // call createNewFridgeItem to create the JSON, and then make the necessary requesthandler call
         
@@ -369,7 +324,6 @@ re.controller = (function() {
             var newItem = createFridgeItem(itemName, expiration, shared);
             re.requestHandler.addItem(newItem, rhAddCallback);
         });
->>>>>>> 1e8a919db95b29df111b7f246810dd41727927ac
     }
     
     
