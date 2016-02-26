9461dd558444d4c7e75e99e416d1fbc6b701095f
OmarAlSughayer <oasugher@gmail.com>
2016-02-25 18:08:26 -0800

    Added groupLoginTests

--- RoomEasePhoneGapProject/www/tests/fbHandlerSpec.js
+++ RoomEasePhoneGapProject/www/tests/fbHandlerSpec.js
@@ -1,37 +1,51 @@
-/** 
-* Example test suite using the Jasmine testing framework
-* Include this file as a dependency in spec.html for local testing
-* or for any automated testing suites
-*/
+
 describe("Facebook Handler suite", function() {
-    it("has successfully loaded jQuery", function() {
-        expect($).toBeTruthy();
-    });
-    
-    it("has successfully loaded re.js", function() {
-        expect(re).toBeTruthy();
-    });
-    
-    it("can find the window", function() {
-        expect(window).toBeTruthy();
-    });
-    
-    describe("is testing template", function() {
-        beforeEach(function() {
-             re.templates.load(["Fridge", "List"]).done(function() {
-                 // TODO: figure out how to test asynchronously so these have
-                 // time to load
-             });
-        });
-        
-        it("for fridge", function() {
-            var fridgeTemplate = re.templates.get("Fridge");
-            expect(fridgeTemplate).toBeTruthy();
-        });
-        
-        it("for list", function() {
-            var listTemplate = re.templates.get("List");
-            expect(listTemplate).toBeTruthy();        
-        });
+    var id = "0123456789";
+    var name = "Nachos";
+    var info = {
+                    'name': name,
+                    'id': id
+               };
+
+    it("Store the user name and id locally", function( ){
+
+        re.fbHandler.moveToGroupLogin(info, null);
+
+        waitsFor(function(){
+            return true;
+        }, "group login never finished", 5000);
+
+        expect(window.localStorage.getItem('user_id')).toEqual(id);
+        expect(window.localStorage.getItem('user_name')).toEqual(name);
     });
 });
+
+
+/*
+describe("Facebook Handler suite", function() {
+    
+    var id = "0123456789";
+    var name = "Omar AlSughayer";
+    var info = {
+                    'name': name,
+                    'id': id
+               };
+
+    it("", function( ){
+        var id = "0123456789";
+        var name = "Omar AlSughayer";
+        var info = {
+                        'name': name;
+                        'id': id;
+                   };
+        re.fb_handler.moveToGroupLogin(info, error);
+
+        waitsFor(function(){
+            return finished;
+        }, "group login never finished", 5000);
+
+        expect(window.localStorage.getItem('user_id')).toEqual(id);
+        expect(window.localStorage.getItem('user_name')).toEqual(name);
+    });
+});
+*/
