255b5744ace2169cfa0ad9b918439ed8179183f9
OmarAlSughayer <oasugher@gmail.com>
2016-02-26 00:20:04 -0800

    Added logout tests

--- RoomEasePhoneGapProject/www/tests/fbHandlerSpec.js
+++ RoomEasePhoneGapProject/www/tests/fbHandlerSpec.js
@@ -40,4 +40,43 @@ describe("Facebook Handler suite", function() {
         expect(window.localStorage.getItem('user_name')).toEqual(name);
     });
 
+    it("deletes the user and group info after logging out", function( ){
+
+        window.localStorage.setItem('user_id', id);
+        window.localStorage.setItem('group_id', id);
+        window.localStorage.setItem('group_name', name);
+        window.localStorage.setItem('group_password', name);
+
+        re.fbHandler.logout(function(){});
+
+        expect(window.localStorage.getItem('user_id')).toBeNull();
+        expect(window.localStorage.getItem('group_id')).toBeNull();
+        expect(window.localStorage.getItem('group_name')).toBeNull();
+        expect(window.localStorage.getItem('group_password')).toBeNull();
+    });
+
+    it("invokes the callback function correctly", function( ){
+        window.localStorage.setItem('user_id', id);
+        window.localStorage.removeItem('test');
+
+        re.fbHandler.logout(function( ){ window.localStorage.setItem('test', name); });
+        expect(window.localStorage.getItem('test')).toEqual(name);
+    });
+
+    it("does not invoke callback function if user was not logged in", function( ){
+        window.localStorage.removeItem('user_id');
+        window.localStorage.removeItem('test');
+
+        re.fbHandler.logout(function( ){ window.localStorage.setItem('test', name); });
+        expect(window.localStorage.getItem('test')).toBeNull(name);
+    });
+
+    it("redirects correctly", function( ){
+        window.localStorage.setItem('user_id', id);
+
+        re.fbHandler.logout(function(){});
+
+        expect(window.location.hash).toEqual("#fb"); 
+    });
+
 });
