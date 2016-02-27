88e55abe6a36c8dff013a3a4571f935c38e905a1
cher-wang <cwang7@uw.edu>
2016-02-26 20:20:39 -0800

    Wrote a skeleton of Fridge test

---
+++ RoomEasePhoneGapProject/www/tests/FridgeControllerSpec.js
@@ -0,0 +1,15 @@
+describe("Fridge Controller suite", function() {   
+    
+    // Load the fridge template into Jasmine html
+    beforeEach(function() {
+        
+        $('body').append();
+    });
+    
+    it("Makes new fridge item", function() {
+        
+        
+    
+        
+    });
+});
