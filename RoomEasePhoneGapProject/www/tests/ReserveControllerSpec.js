94bd7c102798f787a79a5a9a929eca45d9a61413
Matthew Mans <mmans1626@gmail.com>
2016-02-26 20:21:07 -0800

    Added test that adds elements into DOM

--- RoomEasePhoneGapProject/www/tests/ReserveControllerSpec.js
+++ RoomEasePhoneGapProject/www/tests/ReserveControllerSpec.js
@@ -1,16 +1,37 @@
 describe("Reservation Controller Suite", function(){
-    it("Make new reservation", function(){
+    
+//    beforeEach(function(){
+//        re.templates.load(['Reservations']).done(function(){
+//            template = re.templates.get('Reservations');
+//        
+//            $("body").append("<div id='test'></div>");
+//            $('#test').append(template());
+//
+//
+//        });
+//    });
+
+    it("Make new reservation type", function(){
+        var finished = false;
+        re.templates.load(['Reservations']).done(function(){
+            template = re.templates.get('Reservations');
+            $("body").append("<div id='test'></div>");
+            $('#test').html($.parseHTML(template()));
+            console.log($("add-new-reservation-type-btn").children());
+            finished = true;
+        });
         
+        
+        waitsFor(function(){
+			return finished;
+		}, "Failed to add test user to group", 10000);
+
+//        var button = $("#add-new-reservation-type-btn").html(template);
+//        button.trigger("click");
+//        var addType = $("#add-new-resevation-type").html(template);
+//        console.log(addType.css("display"));
     });
     
     it("Edit reservation", function(){
-        beforeEach(function(){
-            re.templates.load(['List']).done(function(){
-                template = re.templates.get('List');
-                console.log("Foo!");
-                console.log(template);
-            });
-            
-        });
     });
 });
