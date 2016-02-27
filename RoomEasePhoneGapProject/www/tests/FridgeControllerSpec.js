b4d82d8e85069587856554ac7f23f04e462b390e
cher-wang <cwang7@uw.edu>
2016-02-26 21:59:43 -0800

    fridge controller spec test update

---
+++ RoomEasePhoneGapProject/www/tests/FridgeControllerSpec.js
@@ -0,0 +1,51 @@
+describe("Fridge Controller suite", function() {
+    
+    describe("Fridge UI allows user to add new fridge item", function() {
+        it("Shows add new fridge item popup", function() {
+            var finished = false;
+            var template;
+
+            re.templates.load(['Fridge']).done(function() {
+                template = re.templates.get('Fridge');
+                $("body").append("<div id='test'></div>");
+                $('#test').html($.parseHTML(template()));
+
+                var addButton = $("#new-fridge-item-btn");
+                addButton.trigger("click");
+                var addButtonDisplay = $('#new-fridge-item-btn').css('display');
+                var popupDisplay = $('.popupBackground').css('display');
+                expect(addButtonDisplay).toEqual("none");
+                expect(popupDisplay).toEqual("block");
+                finished = true;
+            });
+
+            waitsFor(function() {
+                return finished;
+            }, "Failed to show popup to add new fridge item", 10000);     
+        });
+        
+        it ("Prompts user to enter in fridge item name and date of expiry if left blank", function() {
+            var finished = false;
+            var template;
+
+            re.templates.load(['Fridge']).done(function() {
+                template = re.templates.get('Fridge');
+                $("body").append("<div id='test'></div>");
+                $('#test').html($.parseHTML(template()));
+
+                $("#new-fridge-item-btn").trigger("click");
+                
+                var addButtonDisplay = $('#new-fridge-item-btn').css('display');
+                var popupDisplay = $('.popupBackground').css('display');
+                expect(addButtonDisplay).toEqual("none");
+                expect(popupDisplay).toEqual("block");
+                finished = true;
+            });
+
+            waitsFor(function() {
+                return finished;
+            }, "Failed to show popup to add new fridge item", 10000);            
+        });
+    });
+    
+});
