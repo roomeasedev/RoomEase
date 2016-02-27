92dbd87a5e1568614611ea8611b45c8ebe2023ec
Siddhartha Gorti <sgorti3@uw.edu>
2016-02-26 18:59:46 -0800

    removed alert/console.log statements from fridge_controller.js

--- RoomEasePhoneGapProject/www/js/fridge_controller.js
+++ RoomEasePhoneGapProject/www/js/fridge_controller.js
@@ -53,9 +53,7 @@ re.fridge_controller = (function() {
         var newItem = createFridgeItem(itemName, expiration, shared);
         re.requestHandler.addItem(newItem, re.new_controller.rhAddCallback);
         
-        var expDate = new Date(expiration);
-        alert(expDate);
-        
+        var expDate = new Date(expiration);        
         var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
         var diffDays = Math.ceil(Math.abs((expDate.getTime() - new Date().getTime())/oneDay));
         
