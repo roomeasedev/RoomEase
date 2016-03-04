fe2e2b1c6c71336f64f10ae253eafcc9ffa6263b
Jakob Sunde <jsunde@uw.edu>
2016-03-04 12:29:25 -0800

    Removed extraneous, deprecated test users from re.js

--- RoomEasePhoneGapProject/www/js/re.js
+++ RoomEasePhoneGapProject/www/js/re.js
@@ -29,33 +29,13 @@ $(document).ready(function() {
         }
     });*/
 
-    
-    //Either set the values to empty string if you want to be directed to a certain page,
-    // set them to the default test values if you want to bypass the groups/ids and just
-    // go to the default test group, or comment out both and observe the natural flow of
-    // the app.
-    //SET TO EMPTY TO FORCE NEW LOGIN
-    //window.localStorage.setItem("user_id", "");
-    //window.localStorage.setItem("group_id", "");
-    //SET TO TEST VALUES TO GO TO DEFAULT TEST GROUP
     //User 1
     //window.localStorage.setItem("user_id", "795578070");
     //window.localStorage.setItem("user_name", "Nacho Cano");
     //window.localStorage.setItem("group_id", "089d6e77903ccfb44b5bcad1f72f8c88");
     
-    //window.localStorage.setItem("user_id", "802839149821946");
-    //window.localStorage.setItem("user_name", "Sidd Gorti");
-    //window.localStorage.setItem("group_id", "");
-    
-    
-    //User 2
-    //window.localStorage.setItem('user_id', "512963585");
-    //User 3
-    //window.localStorage.setItem('user_id', "743566854");
-    //window.localStorage.setItem("group_id", "089d6e77903ccfb44b5bcad1f7157b47");
     re.newController.init();
+    
     // load templates and render first viewport
     re.render.init();
 });
-
-
