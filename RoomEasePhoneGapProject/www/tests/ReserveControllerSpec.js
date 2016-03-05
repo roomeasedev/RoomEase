709cbaf5eba5272c502b76bfe4629f2c70333940
Matthew Mans <mans1626@uw.edu>
2016-03-05 07:26:35 -0800

    Added ReserveController unit tests

--- RoomEasePhoneGapProject/www/tests/ReserveControllerSpec.js
+++ RoomEasePhoneGapProject/www/tests/ReserveControllerSpec.js
@@ -1,37 +1,118 @@
 describe("Reservation Controller Suite", function(){
-    
-//    beforeEach(function(){
-//        re.templates.load(['Reservations']).done(function(){
-//            template = re.templates.get('Reservations');
-//        
-//            $("body").append("<div id='test'></div>");
-//            $('#test').append(template());
-//
-//
-//        });
-//    });
+        window.location.hash = "reservations";
+        window.localStorage.setItem('group_id', "089d6e77903ccfb44b5bcad1f732caf6");
+        window.localStorage.setItem('user_id', "35668452");
 
-    it("Make new reservation type", function(){
+
+    it("Make new reservation popup", function(){
+        $(".page").remove();
+        window.location.hash = "reservations";
+        window.localStorage.setItem('group_id', "089d6e77903ccfb44b5bcad1f732caf6");
+        window.localStorage.setItem('user_id', "35668452");
         var finished = false;
         re.templates.load(['Reservations']).done(function(){
             template = re.templates.get('Reservations');
-            $("body").append("<div id='test'></div>");
-            $('#test').html($.parseHTML(template()));
-            console.log($("add-new-reservation-type-btn").children());
+            $("body").append("<div class='page'></div>");
+            $('.page').html($.parseHTML(template()));
+            var addButton = $("#new-reservation-btn");
+            addButton.trigger("click");
+            var popupDisplay = $('.popupBackground').css('display');
+            expect(popupDisplay).toEqual("block");
+            $(".page").html('');
             finished = true;
+
         });
         
         
         waitsFor(function(){
 			return finished;
 		}, "Failed to add test user to group", 10000);
-
-//        var button = $("#add-new-reservation-type-btn").html(template);
-//        button.trigger("click");
-//        var addType = $("#add-new-resevation-type").html(template);
-//        console.log(addType.css("display"));
     });
     
-    it("Edit reservation", function(){
+    
+    it("Add new reservation", function(){
+        var finished = false;
+        re.templates.load(['Reservations']).done(function(){            
+            var addButton = $("#new-reservation-btn");
+            addButton.trigger("click");
+            popupDisplay = $('#background1').css('display');
+            expect(popupDisplay).toEqual("block");
+            
+            
+            var now = new Date();
+
+            var day = ("0" + now.getDate()).slice(-2);
+            var month = ("0" + (now.getMonth() + 1)).slice(-2);
+            var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+            $('#start-date').val(today);
+            $("#start-time").val("21:22");
+            $("#reservation-minutes").val("1");
+            $("#reservation-hours").val("1");
+            $("#create-done").trigger("click");
+            finished = true;
+        });
+        
+        waitsFor(function(){
+            
+            //Note: Since the new type is added async, we have to wait for the callback to finally occur for it to be added to the dropdown
+            var allResItems = $(".all-reservation-items");
+			return finished && allResItems.size() == 1;
+		}, "Failed to add test user to group", 10000);
     });
+    
+        
+    it("Add second reservation", function(){
+        var finished = false;
+        re.templates.load(['Reservations']).done(function(){            
+            var addButton = $("#new-reservation-btn");
+            addButton.trigger("click");
+            popupDisplay = $('#background1').css('display');
+            expect(popupDisplay).toEqual("block");
+            
+            
+            var now = new Date();
+
+            var day = ("0" + now.getDate()).slice(-2);
+            var month = ("0" + (now.getMonth() + 1)).slice(-2);
+            var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+            $('#start-date').val(today);
+            $("#start-time").val("23:22");
+            $("#reservation-minutes").val("1");
+            $("#reservation-hours").val("1");
+            $("#create-done").trigger("click");
+            finished = true;
+    });
+        
+        waitsFor(function(){
+            
+            //Note: Since the new type is added async, we have to wait for the callback to finally occur for it to be added to the dropdown
+            var allResItems = $(".all-reservation-items");
+			return finished && allResItems.size() == 2;
+		}, 10000);
+    });
+    
+    it("Delete reservation", function(){
+        var finished = true;
+        $(".all-reservation-items").each(function(index){
+            //Only select the first item for deletion
+            if(index == 0){ 
+                re.reserveController.editReservationItem(this.id);
+                $('#delete-delete').trigger('click');
+                finished = true;
+           } 
+        });
+        
+        waitsFor(function(){
+            
+            //Note: Since the new type is added async, we have to wait for the callback to finally occur for it to be added to the dropdown
+            var allResItems = $(".all-reservation-items");
+            if(finished && allResItems.size() == 1){
+                $(".page").html('');
+                return true;
+            } else {
+                return false;
+            }
+			
+		}, "Failed to add test user to group", 10000);
+    });        
 });
